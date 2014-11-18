cb040e0487cc0445a03833d509c6c30e17a6a586
PGtawasi <higuchinaoyuki@info-no-MacBook-Air-42.local>
2014-11-18 19:55:29 +0900

    Merge branch 'master' of https://github.com/ikageso1/sample1
    Conflicts:
    hogehoge.cpp

--- hogehoge.cpp
+++ hogehoge.cpp
@@ -1,6 +1,14 @@
 int main(){
 <<<<<<< HEAD
+<<<<<<< Updated upstream
  printf("hoge2\n");
+=======
+<<<<<<< HEAD
+ printf("hoge3\n");
+=======
+ printf("hoge2\n");
+>>>>>>> FETCH_HEAD
+>>>>>>> Stashed changes
 =======
  printf("hogehogehogeeeeeeeeeeeeee\n");
 >>>>>>> FETCH_HEAD
